38ccfd006349f3ba2b5bc5802dec434d050e623e
Prachi <prachi.patel@techiegent.com>
2023-02-27 17:26:04 +0530

    w

---
+++ indexloop.js
@@ -0,0 +1,47 @@
+//do while loop
+/*let i = 10;
+do{
+    console.log(i)
+    i++;
+}while(i<15);
+
+
+//for loop
+let i=0;
+for(i=0;i<=5;i++)
+{
+    console.log(i);
+}
+
+//while loop
+let i=1;
+while(i<10)
+{
+    console.log(i);
+    i++;
+}
+
+//for in loop......define string
+const string = 'code';
+
+for (let i in string) {
+    console.log(string[i]);
+}
+
+//for-in loop.....define array
+let arr = {
+    name: ['prachi','jini'],
+    age:[20,21],
+    class: [12,13]
+}
+
+for(let i in arr)
+{
+    console.log(arr[i]);
+}*/
+
+//for-of loop #use for iterable data structure like array,string.
+for(let b of "prachi")
+{
+    console.log(b);
+}
