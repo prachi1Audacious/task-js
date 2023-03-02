83aa858163ac5766d8e9e6735b41e34e00749862
Prachi <prachi.patel@techiegent.com>
2023-03-02 12:30:51 +0530

    sd

--- practice.js
+++ practice.js
@@ -10,7 +10,7 @@
  const add = (a,b,c,d,e)=>{
     return a+b+c+d+e
  };
- console.log(add(...arr));*/
+ console.log(add(...arr));
 
 
  const myName ={fn:"Tarun",
@@ -22,12 +22,14 @@
               city:"Indore"  
 
 }
-console.log(...bio);
+console.log(...bio);*/
 
 //rest
-arr =[1,2,3,4,5]
- const add=(a,b,...rest)=>{
-    return a+b+rest
- };
- console.log(add(...arr));
+function addNumber(a,b,c,...other){
+    console.log(other)
+    return a+b+c;
+}
+const res=addNumber(2,5,6,8,9,7,8,9)
+console.log(res)
 
+//spread
