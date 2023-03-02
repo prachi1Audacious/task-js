362b60f6250aeb94979c3de11b4f5184920121d4
Prachi <prachi.patel@techiegent.com>
2023-03-02 18:27:42 +0530

    spread/rest

--- practice.js
+++ practice.js
@@ -1,36 +1,36 @@
 //rest
-/*arr =[1,2,3,4,5]
+arr =[1,2,3,4,5]
  const add =(a,b,...rest)=>{
     return a+b+rest
  };
  console.log(add(...arr));
 
  //spread
- rr =[1,2,3,4,5]
- const add = (a,b,c,d,e)=>{
+ arr =[1,2,3,4,5]
+ const add2= (a,b,c,d,e)=>{
     return a+b+c+d+e
  };
  console.log(add(...arr));
 
 
- const myName ={fn:"Tarun",
-                ln:"1234"
- }
- const bio = {...myName,
-              fn:"Aman",
-              ln:"1234",
-              city:"Indore"  
-
+//spread function
+function sum(x, y ,z) {
+   console.log(x + y + z);
 }
-console.log(...bio);
 
-//rest
+const num1 = [1, 3, 4, 5];
+
+sum(...num1); // 8
+
+
+
+//rest function
 function addNumber(a,b,c,...other){
     console.log(other)
     return a+b+c;
 }
 const res =addNumber(2,5,6,8,9,7,8,9)
-console.log(res)*/
+console.log(res)
 
  //array object
  const services = [
@@ -41,11 +41,15 @@ console.log(res)*/
    { name:"swati",age:24,salary:5000},
 
  ]  
+    console.log(services);
 
- let total = 0;
- services.forEach(item =>{
-   total +=item.price;
- });
- console.log("Total:",total);
+//  let total = 0;
+//  services.forEach(item =>{
+//    total +=item.price;
+//  });
+//  console.log("Total:",total); //error
+let total =0;
+for(let{salary} of services){
+   total += salary;
 
-
+}console.log(total);
